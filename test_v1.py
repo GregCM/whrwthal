159a4cbccdf5678f37496519fecd1da2c8875b46
Greg Caceres <gregcaceres@gmail.com>
2020-05-12 11:49:22 -0500

    pytest PhraseSearch fix

--- test_v1.py
+++ test_v1.py
@@ -13,6 +13,7 @@ class Test:
         assert len(d['CONCORDANCE']) == 13664
 
     def test_PhraseSearch(*kwargs):
+        Bible.__init__(Bible, configfile='config_test.ini')
         Bible.frame.entry = 'rightly dividing'
         assert len(Bible.PhraseSearch(Bible)['phrases']) == 1
         v1.Bible.frame.entry = 'word'
