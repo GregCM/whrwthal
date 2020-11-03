e3f6eeec416aaf387e6bc5bec4875515b7220cee
GregCM <gregcaceres@gmail.com>
2020-11-02 21:09:30 -0600

    reformat of src.txt into newline separators per verse

--- whrwthal/textile.py
+++ whrwthal/textile.py
@@ -71,5 +71,5 @@ def keybinds(self):
 def about():
     version = 'v2.3.4'
     uilines = '676 GUI Lines of Code'
-    totallines = '1405 Total Lines of Code'
+    totallines = '1408 Total Lines of Code'
     return 'whrwthal version %s' % (version), uilines, totallines
