1a393a9eccd0c4cca695ea7ac5d0a513e72529eb
GregCM <gregcaceres@gmail.com>
2020-11-03 12:34:09 -0600

    backreferenced sed

--- whrwthal/mdat.sh
+++ whrwthal/mdat.sh
@@ -7,10 +7,10 @@
 
 version=$(git tag | tail -1)
 sed -Ei "s/v[0-9].[0-9].[0-9]/$version/" textile.py
-sed -Ei "s/version = '.*'/version = '$version'/" textile.py
+sed -Ei "s/(version = )'.*'/\1'$version'/" textile.py
 
 uilines=$(wc -l handler.py | sed "s/handler.py/GUI Lines of Code/")
-sed -Ei "s/uilines = '.*'/uilines = '$uilines'/" textile.py
+sed -Ei "s/(uilines = )'.*'/\1'$uilines'/" textile.py
 
 lines=$(wc -l *py | sed "s/total/Total Lines of Code/" | grep -P -e "\d+(?= Total.Lines)" | sed "s/^ //")
-sed -Ei "s/totallines = '.*'/totallines = '$lines'/" textile.py
+sed -Ei "s/(totallines = )'.*'/\1'$lines'/" textile.py
