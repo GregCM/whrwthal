49df16167cfed58cc05c6a6a74f22d1ffa993fc8
GregCM <gregcaceres@gmail.com>
2020-11-11 11:34:18 -0600

    book and verse tests added

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -20,18 +20,34 @@ class TestParser(unittest.TestCase):
     whrwthal.__init__(whrwthal)
 
     def test_find(self):
-        phrase_tests = ['therefore', 'wherefore', 'therewith', 'wherewithal']
-        for s, i in zip(phrase_tests, [1220, 344, 36, 2]):
-            d, count = whrwthal.parser.find(whrwthal, s)
-            self.assertEqual(i, count, msg='\n\n[%s] expected %i result(s), got %i'
-                             % (s, i, count))
+        book_tests = ['JOB', 'PROVERBS', 'PHILEMON']
+        for b, i in zip(book_tests, [3445, 2932, 4248]):
+            d, count = whrwthal.parser.find(whrwthal, b)
+            self.assertEqual(i, len(d['%s 1' % (b)]),
+                             msg='\n\n[%s] Expected length %i, got %i'
+                             % (b, i, len(d['%s 1' % (b)])))
 
         chapter_tests = ['PSALMS 117', 'PSALMS 119', 'ROMANS 1']
         for c, i in zip(chapter_tests, [185, 14292, 4114]):
             d, count = whrwthal.parser.find(whrwthal, c)
-            self.assertEqual(i, len(d[c]), msg='\n\n[%s] Expected length %i, got %i'
+            self.assertEqual(i, len(d[c]),
+                             msg='\n\n[%s] Expected length %i, got %i'
                              % (c, i, len(d[c])))
 
+        verse_tests = ['GENESIS 3:9', 'JOB 40:2', 'ROMANS 3:23',
+                       'PHILEMON 1:3', 'REVELATION 22:21']
+        for v, i in zip(verse_tests, [204, 154, 143, 153, 64]):
+            d, count = whrwthal.parser.find(whrwthal, v)
+            self.assertEqual(i, len(d[v]),
+                             msg='\n\n[%s] Expected length %i, got %i'
+                             % (v, i, len(d[v])))
+
+        phrase_tests = ['therefore', 'wherefore', 'therewith', 'wherewithal']
+        for s, i in zip(phrase_tests, [1220, 344, 36, 2]):
+            d, count = whrwthal.parser.find(whrwthal, s)
+            self.assertEqual(i, count,
+                             msg='\n\n[%s] expected %i result(s), got %i'
+                             % (s, i, count))
 
 if __name__ == '__main__':
     unittest.main()
