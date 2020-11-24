5b92ef8762a1662ec5aa3eeb0d05963f900ea3db
GregCM <gregcaceres@gmail.com>
2020-11-24 16:38:07 -0600

    total LOC including submods

--- whrwthal/metadata.sh
+++ whrwthal/metadata.sh
@@ -9,8 +9,5 @@ version=$(git tag | tail -1)
 sed -Ei "s/v[0-9]+.[0-9]+.[0-9]+/$version/" textile.py
 sed -Ei "s/(version = )'.*'/\1'$version'/" textile.py
 
-uilines=$(wc -l tkhandler.py | sed "s/handler.py/GUI Lines of Code/")
-sed -Ei "s/(uilines = )'.*'/\1'$uilines'/" textile.py
-
-lines=$(wc -l *py | sed "s/total/Total Lines of Code/" | grep -P -e "\d+(?= Total.Lines)" | sed "s/^ //")
+lines=$(wc -l ../*/*py | sed "s/total/Total Lines of Code/" | grep -P -e "\d+(?= Total.Lines)" | sed "s/^ //")
 sed -Ei "s/(totallines = )'.*'/\1'$lines'/" textile.py
